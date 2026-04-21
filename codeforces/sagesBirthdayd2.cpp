a53e80d6903e2d028e16ac85b14fdfb336c6e3e4
Chaitanya Rathi <chaitanyarathi29@gmail.com>
2026-04-21 07:25:53 +0530

    3qas

--- codeforces/sagesBirthdayd2.cpp
+++ codeforces/sagesBirthdayd2.cpp
@@ -13,7 +13,24 @@ int main(){
     }    
     sort(v.begin(),v.end());
     
-    
+    vector<ll>ans(n,0);
+    int j=0;
+    for(int i=1;i<n;i+=2){
+        ans[i]=v[j];j++;
+    }
+    for(int i=0;i<n;i+=2){
+        ans[i]=v[j];j++;
+    }
+    int cnt=0;
+    for(int i=1;i<n-1;i++){
+        if(ans[i]<ans[i-1] && ans[i]<ans[i+1]){
+            cnt++;
+        }
+    }
+    cout<<cnt<<endl;
+    for(auto&it:ans){
+        cout<<it<<" ";
+    }
 
     return 0;
 }
