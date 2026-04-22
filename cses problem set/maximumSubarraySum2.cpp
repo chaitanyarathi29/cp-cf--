80b37e0079da06114353466d23da64fb13d8854d
Chaitanya Rathi <chaitanyarathi29@gmail.com>
2026-04-23 01:34:20 +0530

    cses

--- cses problem set/maximumSubarraySum2.cpp
+++ cses problem set/maximumSubarraySum2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm>
 using ll = long long;
 using namespace std;
 
@@ -12,8 +14,24 @@ int main(){
     for(auto&it:v){
         cin>>it;
     }
+    for(int i=1;i<n;i++){
+        v[i]+=v[i-1];
+    }
+    multiset<ll>m;
+    for(int i=a-1;i<b;i++){
+        m.insert(v[i]);
+    }
+    ll ans =*m.rbegin();
+    int left=a-1,right=b;
+    for(int i=1;i<=n-a;i++){
+        auto it = m.find(v[left]);
+        m.erase(it);
+        if(right<n) m.insert(v[right]);
+        ans=max(ans,*m.rbegin()-v[i-1]);
+        left++;right++;
+    }
+    
+    cout<<ans<<endl;
 
-    
-    
     return 0;
 }
